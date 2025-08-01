8bfc2ccc12f285c95be309351b0e4106eedaf979
Your Name <your-email@example.com>
2025-08-01 02:17:09 -0700

    ali

--- src/pages/SearchAddUser.jsx
+++ src/pages/SearchAddUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { collection, query, where, getDocs } from "firebase/firestore";
